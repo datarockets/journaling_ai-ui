6b7320b1fad770a2798058be059bb01daf13d886
Elvin Bayramov <e.bayramov@datarockets.com>
2023-12-21 23:46:34 +0400

    Update layout to better suit mobile-first design

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -16,23 +16,30 @@
 		DotsVerticalOutline
 	} from 'flowbite-svelte-icons';
 	import '../../app.pcss';
+
+	const backAvailable = true // TODO: Get data from router, hide for Home
+	const actions = [1] // TODO: Change to get data depending on the page
 </script>
 
-<div class="flex min-h-screen flex-col p-5">
-	<header class="flex items-center gap-8">
+<div class="flex min-h-screen flex-col">
+	<header class="flex items-center gap-4 bg-secondary-900 p-4">
+		{#if backAvailable}
 		<Button outline pill class="!p-2">
 			<ArrowLeftOutline class="h-4 w-4" />
 		</Button>
-		<Heading tag="h2">Hello, @username!</Heading>
-		<Button>
-			<DotsVerticalOutline size="lg" class="h-4 w-4" />
-		</Button>
-		<Dropdown>
-			<DropdownItem>Edit</DropdownItem>
-			<DropdownItem>Remove</DropdownItem>
-		</Dropdown>
+		{/if}
+		<Heading tag="h5">Hello, @username!</Heading>
+		{#if actions.length > 0}
+			<button class="dots-menu">
+				<DotsVerticalOutline size="lg" class="h-4 w-4" />
+			</button>
+			<Dropdown triggeredBy=".dots-menu">
+				<DropdownItem>Edit</DropdownItem>
+				<DropdownItem>Remove</DropdownItem>
+			</Dropdown>
+		{/if}
 	</header>
-	<main class="grow p-16">
+	<main class="grow py-5 px-4">
 		<slot />
 	</main>
 	<Footer>
