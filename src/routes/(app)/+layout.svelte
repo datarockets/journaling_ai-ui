aa03b287f4db8984bfed3e7ed32a978808b23540
Karina Grishaeva <revuwem@gmail.com>
2023-12-22 00:18:36 +0400

    feat(routes/(app)): add padding for footer ButtonGroup

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,13 +1,5 @@
 <script>
-	import {
-		Button,
-		ButtonGroup,
-		Dropdown,
-		DropdownItem,
-		Footer,
-		Heading,
-		Navbar
-	} from 'flowbite-svelte';
+	import { Button, ButtonGroup, Dropdown, DropdownItem, Footer, Heading } from 'flowbite-svelte';
 	import {
 		HomeSolid,
 		CalendarMonthSolid,
@@ -17,16 +9,16 @@
 	} from 'flowbite-svelte-icons';
 	import '../../app.pcss';
 
-	const backAvailable = true // TODO: Get data from router, hide for Home
-	const actions = [1] // TODO: Change to get data depending on the page
+	const backAvailable = true; // TODO: Get data from router, hide for Home
+	const actions = [1]; // TODO: Change to get data depending on the page
 </script>
 
 <div class="flex min-h-screen flex-col">
-	<header class="flex items-center gap-4 bg-secondary-900 p-4">
+	<header class="bg-secondary-900 flex items-center gap-4 p-4">
 		{#if backAvailable}
-		<Button outline pill class="!p-2">
-			<ArrowLeftOutline class="h-4 w-4" />
-		</Button>
+			<Button outline pill class="!p-2">
+				<ArrowLeftOutline class="h-4 w-4" />
+			</Button>
 		{/if}
 		<Heading tag="h5">Hello, @username!</Heading>
 		{#if actions.length > 0}
@@ -39,11 +31,11 @@
 			</Dropdown>
 		{/if}
 	</header>
-	<main class="grow py-5 px-4 flex flex-col">
+	<main class="flex grow flex-col px-4 py-5">
 		<slot />
 	</main>
 	<Footer>
-		<ButtonGroup class="flex justify-center">
+		<ButtonGroup class="flex justify-center py-5">
 			<Button>
 				<HomeSolid class="me-2 h-3 w-3" />
 				Home
