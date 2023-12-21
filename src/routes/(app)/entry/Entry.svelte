eb8deaeb9f9196015e5d631f6df21aa2517ed773
Karina Grishaeva <revuwem@gmail.com>
2023-12-22 01:25:40 +0400

    feat: Journal entry page - UI

---
+++ src/routes/(app)/entry/Entry.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { Textarea, Label, Button, P, Heading } from 'flowbite-svelte';
+
+	export let viewMode = false;
+	export let note = 'My daily note';
+</script>
+
+<div class="mb-12">
+	{#if viewMode}
+		<div class="rounded border p-5">
+			<P>{note}</P>
+		</div>
+	{:else}
+		<Label for="entry">How was your day?</Label>
+		<Textarea id="entry" placeholder="Your message" rows="6" name="message">
+			{note}
+			<div slot="footer" class="grid">
+				<Button class="w-full max-w-32 justify-self-end">Post</Button>
+			</div>
+		</Textarea>
+	{/if}
+</div>
+<!-- Mem of the day -->
+<div class="mx-auto max-w-lg">
+	<img src="https://app.supermeme.ai/static/login.png" alt="" />
+</div>
